97ae4c0c296127e3eb55643519f80810a4790e5d
vule <vulehuy2905@gmail.com>
2022-12-14 22:39:14 +0700

    feat: add viper to load env config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,20 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:P@ssword123@localhost:5432/my_blog?sslmode=disable"
+	"github.com/vule96/my-blog-be/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config ", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db ", err)
 	}
--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/vule96/my-blog-be/api"
 	db "github.com/vule96/my-blog-be/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:P@ssword123@localhost:5432/my_blog?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/vule96/my-blog-be/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database ", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server ", err)
 	}
